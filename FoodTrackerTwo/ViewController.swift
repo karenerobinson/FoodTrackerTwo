392252f78f7baff95fe88107386c6cf24bc20c30
Karen E. Robinson <karenerobinson@kerobinso-mbpro.lan>
2018-07-05 18:04:47 -0700

    Initial Commit

---
+++ FoodTrackerTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTrackerTwo
+//
+//  Created by Karen E. Robinson on 7/5/18.
+//  Copyright © 2018 No Frills Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
