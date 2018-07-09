848e3b0924785396622f9cc2ff410502ade655af
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-09 16:17:17 -0700

    Connect the UI to Code / checkpoint 1

--- FoodTrackerTwo/ViewController.swift
+++ FoodTrackerTwo/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealNameText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text="new default"
+    }
+    
+    
 }
 
