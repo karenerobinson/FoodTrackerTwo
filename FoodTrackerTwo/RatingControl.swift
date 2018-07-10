5fcf3b2cdd23183081c687b9cabb0ec4815755dc
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-09 21:36:58 -0700

    End of "Work with View Controllers"

---
+++ FoodTrackerTwo/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTrackerTwo
+//
+//  Created by Karen E. Robinson on 7/9/18.
+//  Copyright © 2018 No Frills Apps. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
