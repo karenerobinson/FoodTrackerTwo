0ab09c9812b1bb132b5e011da1ae2f83dbc30c56
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-10 10:14:00 -0700

    "Create a Table View" 2nd checkpoint

---
+++ FoodTrackerTwo/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerTwo
+//
+//  Created by Karen E. Robinson on 7/10/18.
+//  Copyright © 2018 No Frills Apps. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
