2976e5fefe970776e3eecb656ff497d1c113b9cd
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-10 10:34:05 -0700

    "Create a Table View" 3rd checkpoint - now we're seeing data, UI needs cleanup.

--- FoodTrackerTwo/MealTableViewController.swift
+++ FoodTrackerTwo/MealTableViewController.swift
@@ -23,6 +23,11 @@ class MealTableViewController: UITableViewController {
         // Load the sample data.
         loadSampleMeals()
     }
+    
+    // need these three:
+    // func numberOfSections(in tableView: UITableView) -> Int
+    // func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    // func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -32,24 +37,32 @@ class MealTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCellID" //testing the "ID" ending here.
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        } //*** wish I understood this use of 'guard'
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
