7ed433d7f54a5e98cbdef1dd9e38e11a3c92de5b
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-10 09:05:38 -0700

    "Implement a Custom Control" 7th (?) (last) checkpoint

--- FoodTrackerTwo/ViewController.swift
+++ FoodTrackerTwo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var mealNameText: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     
     override func viewDidLoad() {
@@ -61,9 +62,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     //MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameText.text="new default"
-    }
     
     @IBAction func selectingImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         
