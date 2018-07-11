86f843e831c2ece945eb89a53c2e33d3bae38350
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-10 23:21:35 -0700

    "Implement Edit and Delete" again.  Editing, canceling an edit, both work.

--- FoodTrackerTwo/MealTableViewController.swift
+++ FoodTrackerTwo/MealTableViewController.swift
@@ -161,10 +161,20 @@ class MealTableViewController: UITableViewController {
     
     //MARK: Actions
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.source as? MealSingleViewController, let meal = sourceViewController.meal { //accepted two fixes in this line
-            let newIndexPath = IndexPath(row: meals.count, section: 0)
-            meals.append(meal)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        if let sourceViewController = sender.source as? MealSingleViewController, let meal = sourceViewController.meal {
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else {
+                // Add a new meal.
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
+                
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
         }
     }
     
