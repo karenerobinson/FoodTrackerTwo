728ec89bbeafaaa79be01c5edda4278eeca10d2e
Karen E. Robinson <karenerobinson@kerobinso-mbpro.local>
2018-07-10 23:39:49 -0700

    "Implement Edit and Delete Behavior" - deleting works now.

--- FoodTrackerTwo/MealSingleViewController.swift
+++ FoodTrackerTwo/MealSingleViewController.swift
@@ -92,7 +92,20 @@ class MealSingleViewController: UIViewController, UITextFieldDelegate, UIImagePi
     //MARK: Navigation
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
+    
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     
